b0c7ea77b316967dced154d02bb01866347480d9
Ti-Chung Cheng <tcheng10@illinois.edu>
2021-05-29 08:12:13 +0800

    fix(survey): merge surveys and survey
    Merge surveys ans survey controller together.

--- src/surveys/surveys.controller.ts
+++ src/surveys/surveys.controller.ts
@@ -2,16 +2,44 @@ import {
   Controller,
   Get,
   Post,
-  Patch,
   Delete,
   Body,
   Param,
+  Put,
 } from '@nestjs/common';
+import { CreateSurveyDto } from './dtos/createSurvey.dto';
+import { UpdateSurveyDto } from './dtos/updateSurvey.dto';
+import { SurveysService } from './surveys.service';
 
 @Controller('surveys')
 export class SurveysController {
+  constructor(private surveyService: SurveysService) {}
+
   @Get()
   getUserSurveys(): string {
     return 'This action returns all surveys. Intended for internal use only. (getUserSurveys)';
+  }
+
+  @Get(':id')
+  getSurveyById(@Param('id') id: string) {
+    return this.surveyService.findSurveyById(id);
+  }
+
+  @Post()
+  createSurveyReturnId(@Body() createSurveyDto: CreateSurveyDto) {
+    return this.surveyService.createNewSurvey(createSurveyDto);
+  }
+
+  @Put(':id')
+  updateSurveyById(
+    @Param('id') id: string,
+    @Body() updateSurveyDto: UpdateSurveyDto,
+  ) {
+    return this.surveyService.updateSurveyById(id, updateSurveyDto);
+  }
+
+  @Delete(':id')
+  removeSurveyById(@Param('id') id: string) {
+    return this.surveyService.removeSurveyById(id);
   }
 }
--- src/surveys/surveys.module.ts
+++ src/surveys/surveys.module.ts
@@ -3,14 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Survey, SurveySchema } from 'src/schemas/survey.schema';
 import { SurveysService } from './surveys.service';
 import { SurveysController } from './surveys.controller';
-import { SurveyController } from './survey.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Survey.name, schema: SurveySchema }]),
   ],
   providers: [SurveysService],
-  controllers: [SurveysController, SurveyController],
+  controllers: [SurveysController],
   exports: [SurveysService],
 })
 export class SurveysModule {}
