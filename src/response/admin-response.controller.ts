3fdaabbdafb82dc39da979089ef6764224dcfe2e
Ti-Chung Cheng <tcheng10@illinois.edu>
2021-06-24 15:23:03 +0800

    feat(response): add placeholders for apis
    added placeholders for to be implemented response apis

--- src/response/admin-response.controller.ts
+++ src/response/admin-response.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, UseGuards, Request, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  Query,
+  NotImplementedException,
+  Delete,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Role } from 'src/auth/roles/role.enum';
 import { Roles } from 'src/auth/roles/roles.decorator';
@@ -18,6 +26,7 @@ export class AdminResponseController {
     return this.adminResponseService.getAllSurveyResponses(userid, query);
   }
 
+  // TODO: function not completely implemented
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Get('get-all-question-responses')
@@ -25,7 +34,47 @@ export class AdminResponseController {
     const userid = req.user.userId;
     return this.adminResponseService.getAllQuestionResponses(userid, query);
   }
+
+  @Get('get-all-responses')
+  getAllResponses() {
+    throw new NotImplementedException();
+  }
 }
 
 @Controller('designer')
-export class DesignerResponseController {}
+export class DesignerResponseController {
+  @Get('get-survey-responses/:surveyId')
+  getSurveyResponsesById() {
+    throw new NotImplementedException();
+  }
+
+  @Get('get-question-responses/:questionId')
+  getQuestionResponsesById() {
+    throw new NotImplementedException();
+  }
+
+  @Get('get-full-responses/:surveyId')
+  getFullResponsesById() {
+    throw new NotImplementedException();
+  }
+
+  @Get('get-unique-responses/:key')
+  getUniqueResponsesByKey() {
+    throw new NotImplementedException();
+  }
+
+  @Get('get-skey-responses/:skey')
+  getResponsesBySKey() {
+    throw new NotImplementedException();
+  }
+
+  @Delete('remove-survey-response/:surveyResponseId')
+  removeSurveyResponseById() {
+    throw new NotImplementedException();
+  }
+
+  @Get('remove-question-response/')
+  removeQuestionResponseById() {
+    throw new NotImplementedException('not supported by design.');
+  }
+}
